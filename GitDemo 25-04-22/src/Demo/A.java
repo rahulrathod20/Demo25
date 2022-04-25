fedab6e63c8854271d76d3315fe07a5941502244
rahulrathod20 <104188895+rahulrathod20@users.noreply.github.com>
2022-04-25 12:41:18 +0530

    by rr

--- GitDemo 25-04-22/src/Demo/A.java
+++ GitDemo 25-04-22/src/Demo/A.java
@@ -7,6 +7,11 @@ public class A {
 		System.out.println("git m1 method");
 	}
 	
+	public void m2()
+	{
+		System.out.println("git m2 method");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("main method");

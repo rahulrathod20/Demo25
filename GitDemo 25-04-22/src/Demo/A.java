39e5894d2d8c82b0898d74c727752bffa784e7e4
RAHUL <RAHUL@DESKTOP-NP6VH1C>
2022-04-25 12:43:37 +0530

    by rahul2

--- GitDemo 25-04-22/src/Demo/A.java
+++ GitDemo 25-04-22/src/Demo/A.java
@@ -12,12 +12,18 @@ public class A {
 		System.out.println("git m2 method");
 	}
 	
+	public void m3()
+	{
+		System.out.println("git m3 method");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("main method");
 		
 		A a=new A();
 		 a.m1();
+		 a.m2();
 		
 	}
 }

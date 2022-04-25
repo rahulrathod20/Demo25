ec6ed8d4356abdc776e7d8e1cfa9ffa8673ea458
RAHUL <RAHUL@DESKTOP-NP6VH1C>
2022-04-25 12:35:12 +0530

    by rahul25/04

---
+++ GitDemo 25-04-22/src/Demo/A.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class A {
+
+	public void m1()
+	{
+		System.out.println("git m1 method");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("main method");
+		
+		A a=new A();
+		 a.m1();
+		
+	}
+}
